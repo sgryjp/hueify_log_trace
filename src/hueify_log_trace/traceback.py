06bce1a6e3a1ce2119549a931cdc93bbfefcc5c3
Suguru Yamamoto <sgryjp@gmail.com>
2025-06-03 01:01:16 +0900

    feat: More flexible stack frame filtering

--- src/hueify_log_trace/traceback.py
+++ src/hueify_log_trace/traceback.py
@@ -21,16 +21,90 @@ from __future__ import annotations
 import io
 import os
 import re
-from functools import lru_cache
-from pathlib import Path
-from re import Pattern
 from traceback import FrameSummary
-from typing import IO, TYPE_CHECKING, overload
+from typing import IO, TYPE_CHECKING, Literal, NamedTuple, overload
 
 from termcolor import colored
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
+    from collections.abc import Iterable
+
+
+TracebackFilterEntry = tuple[Literal["show", "hide"], str | re.Pattern[str]]
+"""A type alias for a tuple representing a single entry in the traceback filter."""
+
+
+class _CompiledTracebackFilterEntry(NamedTuple):
+    show: bool
+    pattern: re.Pattern[str]
+
+
+class TracebackFilter:
+    r"""A class to filter stack frames in a traceback.
+
+    This class allows you to specify which stack frames should be shown or hidden
+    based on regular expression patterns. Entries in the filter are evaluated in order,
+    and the first matching entry determines whether a frame is shown or hidden.
+    If no entries match, the frame is shown by default.
+
+    The matching is done by calling the `search` method of each regular expression
+    pattern against the absolute file path of the stack frame.
+
+    The absolute file path of the stack frame to be filtered typically varies depending
+    on the execution environment. Therefore, it is recommended to create filtering
+    conditions that reflect the runtime environment.
+    For example, in an application that uses the ``requests`` package, to hide stack
+    frames from the standard library and site-packages, you can specify the
+    ``filter_rule`` with entries as follows.
+    However, note that in this example the application itself is not excluded if it is
+    installed in site-packages.
+
+    .. code-block:: python
+        import logging
+        from pathlib import Path
+
+        import requests
+
+        import my_app
+
+        stdlib_path = Path(logging.__file__).parents[1]
+        site_packages_path = Path(requests.__file__).parents[1]
+        filter_rule = [
+            ("allow", re.escape(str(site_packages_path.joinpath("my_app")))),
+            ("hide", re.escape(str(stdlib_path))),
+            ("hide", re.escape(str(site_packages_path))),
+        ]
+
+    """
+
+    def __init__(self, entries: Iterable[TracebackFilterEntry]) -> None:
+        """Initialize the TracebackFilter with a list of filter rule entries."""
+        self.entries = [
+            _CompiledTracebackFilterEntry(
+                type_ == "show",
+                re.compile(pattern) if isinstance(pattern, str) else pattern,
+            )
+            for type_, pattern in entries
+        ]
+
+    def evaluate(self, filename: str) -> bool:
+        """Evaluate whether a stack frame should be shown or not based on its filename.
+
+        Parameters
+        ----------
+        filename : str
+            The absolute file path of the stack frame.
+
+        Returns
+        -------
+        bool
+            Whether the frame should be shown or not.
+
+        """
+        for show, pattern in self.entries:
+            if pattern.search(filename):
+                return show
+        return True
 
 
 @overload
@@ -38,39 +112,20 @@ def format_exception(
     exc: BaseException,
     *,
     file: IO[str],
-    allow_list: Sequence[Pattern[str] | str] | None = None,
-    deny_list: None = None,
-) -> None: ...
-@overload
-def format_exception(
-    exc: BaseException,
-    *,
-    file: IO[str],
-    allow_list: None = None,
-    deny_list: Sequence[Pattern[str] | str] | None = None,
+    traceback_filter: TracebackFilter | None = None,
 ) -> None: ...
 @overload
 def format_exception(
     exc: BaseException,
     *,
     file: None = None,
-    allow_list: Sequence[Pattern[str] | str] | None = None,
-    deny_list: None = None,
-) -> str: ...
-@overload
-def format_exception(
-    exc: BaseException,
-    *,
-    file: None = None,
-    allow_list: None = None,
-    deny_list: Sequence[Pattern[str] | str] | None = None,
+    traceback_filter: TracebackFilter | None = None,
 ) -> str: ...
 def format_exception(
     exc: BaseException,
     *,
     file: IO[str] | None = None,
-    allow_list: Sequence[Pattern[str] | str] | None = None,
-    deny_list: Sequence[Pattern[str] | str] | None = None,
+    traceback_filter: TracebackFilter | None = None,
 ) -> str | None:
     """Format exception information with colored output and optional filtering.
 
@@ -78,14 +133,10 @@ def format_exception(
     format to the standard library's ``traceback.format_exception``, but with added
     colorization for better readability in terminal environments.
 
-    The filtering process is as follows:
-
-    1. If ``allow_list`` is provided, only stack frames of which absolute file paths
-       match any of the patterns in ``allow_list`` will be included.
-    2. If ``deny_list`` is provided, stack frames of which absolute file paths match
-       any of the patterns in ``deny_list`` will be excluded from the output.
-    3. If both ``allow_list`` and ``deny_list`` are provided, this function will raise a
-       ``ValueError``.
+    If ``traceback_filter`` is provided, it will be used for filtering the stack frames
+    based on the specified rules. The filtering rules are evaluated in the order they
+    are provided, and the first matching rule determines whether a frame is shown or
+    hidden. If no rules match, the frame is shown by default.
 
     Note that the path separators in the file paths are **normalized to forward slashes
     (`/`)** for consistency across different platforms. Specifically, when checking
@@ -101,12 +152,14 @@ def format_exception(
         Optional file-like object to write to. If ``None``, returns a string.
         Defaults to ``None``.
 
-    allow_list : Sequence[Pattern[str] | str] | None
-        Optional sequence of regular expression patterns to filter stack frames.
-        Defaults to ``None``.
+    filter_rule : Iterable[TracebackFilterEntry] | None
+        Optional sequence of filter rules to apply to the stack frames.
 
-    deny_list : Sequence[Pattern[str] | str] | None
-        Optional sequence of regular expression patterns to filter stack frames.
+        Each entry should be a tuple of the form ``(type, pattern)``, where the ``type``
+        is either ``"show"`` or ``"hide"``, and the ``pattern`` is a regular expression
+        that matches the absolute file path of the stack frame.
+
+        If ``None``, no filtering is applied, and all stack frames are shown.
         Defaults to ``None``.
 
     Returns
@@ -116,18 +169,11 @@ def format_exception(
         ``None``, otherwise `None`.
 
     """
-    if allow_list is not None and deny_list is not None:
-        msg = "Cannot use both `allow_list` and `deny_list` at the same time."
-        raise ValueError(msg)
-
-    if deny_list is not None:
-        raise NotImplementedError  # TODO: Implement deny_list filtering
-
     if file is None:
         with io.StringIO() as buffer:
-            _format_exception(exc, file=buffer, allow_list=allow_list)
+            _format_exception(exc, file=buffer, traceback_filter=traceback_filter)
             return buffer.getvalue()
-    _format_exception(exc, file=file, allow_list=allow_list)
+    _format_exception(exc, file=file, traceback_filter=traceback_filter)
     return None
 
 
@@ -135,7 +181,7 @@ def _format_exception(
     exc: BaseException,
     *,
     file: IO[str],
-    allow_list: Sequence[Pattern[str] | str] | None = None,
+    traceback_filter: TracebackFilter | None = None,
 ) -> None:
     file.write("Traceback (most recent call last):\n")
 
@@ -156,7 +202,7 @@ def _format_exception(
             colored(name, color="magenta"),
         )
         file.write(f"  {linked_location}\n")
-        if allow_list is None or _check_allow_list(allow_list, filename):
+        if traceback_filter is not None and traceback_filter.evaluate(filename):
             line = FrameSummary(filename, lineno, name).line
             file.write("    {}\n".format(colored(line, color="dark_grey")))
 
@@ -167,14 +213,3 @@ def _format_exception(
     )
     file.write(colored(f"{type_name}: {exc!s}", color="red"))
     file.flush()
-
-
-@lru_cache(maxsize=128)
-def _check_allow_list(allow_list: Sequence[Pattern[str] | str], filename: str) -> bool:
-    path = Path(filename).absolute().as_posix()  # Normalize the path for consistency
-    return any(pattern.search(path) for pattern in _compile_all(allow_list))
-
-
-def _compile_all(allow_list: Sequence[Pattern[str] | str]) -> Sequence[Pattern[str]]:
-    # Note that compiled regex patterns are cached in Python.
-    return [re.compile(s) for s in allow_list]
